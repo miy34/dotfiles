28d7dda20165e0286d9fd0a3b5d6c0b5096b30f1
miy34 <gh@miy34.com>
2025-09-18 08:50:51 +0200

    feat(nvim): add LSP support and changed colors

--- nvim_minimal/colors/miy.lua
+++ nvim_minimal/colors/miy.lua
@@ -7,10 +7,23 @@ vim.g.colors_name = "miy"
 
 local hl = vim.api.nvim_set_hl
 
+local colors = {
+  bg       = "#1B1B2B", 
+  fg       = "#C0A8CF", 
+  red      = "#F07078", 
+  green    = "#90B080", 
+  yellow   = "#E2B45C", 
+  blue     = "#7AA2F7", 
+  purple   = "#C080D0", 
+  cyan     = "#70C0D0", 
+  orange   = "#FF9E64", 
+  pink     = "#FF85A0", 
+}
+
 -- Define highlight groups
-hl(0, "Normal",   { fg = "#D27E99", bg = "#16161D" })
-hl(0, "Comment",  { fg = "#7FB4CA", italic = true })
-hl(0, "String",   { fg = "#C34043" })
+hl(0, "Normal",   { fg=colors.purple })
+hl(0, "Comment",  { fg=colors.cyan, italic = true })
+hl(0, "String",   { fg=colors.pink })
 
 -- link groups
 hl(0, "Constant", { link = "String" })
@@ -20,17 +33,38 @@ hl(0, "Number",   { link = "String" })
 hl(0, "StatusLine",   { fg = "#DCD7BA", bg = "#1F1F28" })
 hl(0, "StatusLineNC", { fg = "#727169", bg = "#16161D" })
 
+-- diagnostics
+hl(0, "DiagnosticError", { fg = colors.red })
+hl(0, "DiagnosticWarn", { fg = colors.yellow })
+
 -- dim line numbers and highlight current line
 hl(0, "LineNr",      { fg = "#3C3C44" })
 hl(0, "CursorLineNr",{ fg = "#DCD7BA", bold = true })
 hl(0, "CursorLine",  { bg = "#1F1F28" })
 
+-- Floating windows (hover, signature help, completion, etc.)
+hl(0, "NormalFloat", { fg = "#D27E99", bg = colors.bg })
+hl(0, "FloatBorder", { fg = "#7FB4CA", bg = colors.bg })
+-- Specifically for LSP hover/signature help windows
+hl(0, "LspFloatWinNormal", { fg = "#D27E99", bg = colors.bg })
+hl(0, "LspFloatWinBorder", { fg = "#7FB4CA", bg = colors.bg })
+
+-- Completion menu (if you use omnifunc or cmp without extra styling)
+hl(0, "Pmenu",      { fg = "#D27E99", bg = "#1F1F28" })
+hl(0, "PmenuSel",   { fg = "#1F1F28", bg = "#7FB4CA" })
+
+hl(0, "markdownH1", { fg = colors.purple })
+hl(0, "markdownH2", { fg = colors.pink })
+hl(0, "markdownH3", { fg = colors.orange })
+hl(0, "markdownH4", { fg = colors.purple })
+hl(0, "markdownH5", { fg = colors.purple })
+hl(0, "markdownH6", { fg = colors.purple })
+hl(0, "Todo", { fg = colors.bg, bg = colors.purple })
 
 local groups_to_neutralize = {
-  "Function", "Identifier", "Statement", "Type", "PreProc", "Keyword",
+  "Function", "Identifier", "Statement", "Type", "PreProc",
   "Constant", "Special", "Operator", "Label", "Repeat", "Conditional",
-  "Structure", "Include", "Define", "Todo", "Delimiter", "markdownH1",
-  "markdownH2", "markdownH3", "markdownH4", "markdownH5", "markdownH6"
+  "Structure", "Include", "Define", "Delimiter"
 }
 
 for _, group in ipairs(groups_to_neutralize) do
--- nvim_minimal/init.lua
+++ nvim_minimal/init.lua
@@ -10,7 +10,7 @@ vim.opt.expandtab = true
 vim.opt.cursorline = true
 vim.opt.title = true
 vim.opt.conceallevel = 1
-vim.opt.showcmd = false
+vim.opt.showcmd = true
 vim.opt.clipboard:append { 'unnamedplus' }
 vim.opt.shada = ""  -- disable shada file. No need to keep commands/jump lists or buffers around 
 vim.opt.hlsearch = false
@@ -23,6 +23,13 @@ vim.opt.wildmenu = true
 vim.g.netrw_fastbrowse = 0 -- makes it so that the netwr buffer is closed after use
 
 vim.cmd.colorscheme("miy")
+
+-- disable built-in treesitter 
+vim.api.nvim_create_autocmd("FileType", {
+  callback = function(args)
+    vim.treesitter.stop(args.buf)
+  end,
+})
 
 -- custom functions
 
@@ -49,6 +56,33 @@ function PopulateQuickfixFromSelection()
     else
         print("No Markdown links found!")
     end
+end
+
+function toggle_quickfix() 
+  local qf_open = false
+  for _, win in pairs(vim.api.nvim_list_wins()) do
+    if vim.api.nvim_win_get_config(win).relative == "" and vim.fn.getwininfo(win)[1].quickfix == 1 then
+      qf_open = true
+      break
+    end
+  end
+  if qf_open then
+    vim.cmd("cclose")
+  else
+    vim.cmd("copen")
+  end
+end
+
+function restart_lsp() 
+  local bufnr = vim.api.nvim_get_current_buf()
+  local clients = vim.lsp.get_clients({ bufnr = bufnr })
+
+  for _, client in ipairs(clients) do
+    -- Stop the client
+    client.stop(true)
+    -- Start it again with the same configuration
+    vim.lsp.start_client(client.config)
+  end
 end
 
 function current_date()
@@ -95,6 +129,118 @@ vim.api.nvim_create_user_command("QuickfixFromSelection", PopulateQuickfixFromSe
 vim.api.nvim_create_user_command("CurrentDate", current_date, {})
 vim.api.nvim_create_user_command("OpenMarkdownItem", open_markdown_item, {})
 
--- keymaps
-vim.keymap.set("n", "<leader><t>", current_date, { noremap = false })
+-- LSP 
+vim.lsp.config('*', {
+    root_markers = { '.git', '.jj' },
+    on_attach = function(client, bufnr)
+      client.server_capabilities.semanticTokensProvider = nil
+    end,
+})
+
+vim.lsp.config('rust-analyzer', {
+    cmd = { 'rust-analyzer' },
+    filetypes = {'rust'},
+    settings = {
+      ["rust-analyzer"] = {
+          check = {
+              command = "clippy",
+              extraArgs = { "--no-deps" },
+          },
+          cargo = {
+              allFeatures = true,  -- optional
+          },
+      }
+  }
+})
+
+vim.lsp.config('marksman', {
+    cmd = { 'marksman' },
+    filetypes = {'markdown'},
+})
+
+vim.lsp.enable({'rust-analyzer', 'marksman'})
+
+vim.api.nvim_create_autocmd("DiagnosticChanged", {
+    callback = function() 
+      vim.diagnostic.setqflist({nr=1, open=false}, "r")
+      update_status_line()
+    end,
+})
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+    pattern={"*.rs"},
+    callback=function() vim.lsp.buf.format({async=false}) end,
+}) 
+
+local function hover()
+  vim.lsp.buf.hover(vim.tbl_extend("force", { border = "single" }, {}))
+end
+
+-- Keymaps
+  -- util 
+vim.keymap.set("n", "<leader>t", current_date, { noremap = false })
 vim.keymap.set("n", "gx", open_markdown_item, { noremap = false })
+  -- lsp
+vim.keymap.set("n", "gk", hover,{ noremap = false })
+vim.keymap.set("n", "dk", vim.diagnostic.open_float,  { noremap = false })             
+vim.keymap.set("n", "ga", vim.lsp.buf.code_action,  { noremap = false })             
+vim.keymap.set("n", "gd", vim.lsp.buf.definition, { noremap = false })   
+vim.keymap.set("n", "gi", vim.lsp.buf.references, { noremap = false })  
+vim.keymap.set("n", "gr", vim.lsp.buf.rename, { noremap = false }) 
+vim.keymap.set("i", "<C-s>", vim.lsp.buf.signature_help, { noremap = false })
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, { noremap = false })
+vim.keymap.set("n", "<leader>rr", restart_lsp, { noremap = false })
+vim.keymap.set("i", "<C-space>", "<C-x><C-o>", { noremap = false })
+
+  -- quickfix
+vim.keymap.set("n", "dn", ":silent! cnext<CR>", { noremap = false })
+vim.keymap.set("n", "dp", ":silent! cprev<CR>", { noremap = false })
+vim.keymap.set("n", "dc", ":cclose<CR>", { noremap = false })
+vim.keymap.set("n", "dl", toggle_quickfix, { noremap = false })
+
+-- diagnostics 
+vim.diagnostic.config({
+    virtual_text = false,
+    signs = true,
+    update_in_insert = false,
+    underline = true,
+    severity_sort = true,
+    float = {
+        focusable = true,
+        style = 'minimal',
+        border = 'rounded',
+        source = 'always',
+        header = '',
+        prefix = '',
+    },
+    signs = {
+        text = {
+            [vim.diagnostic.severity.ERROR] = '✘',
+            [vim.diagnostic.severity.WARN] = '▲',
+            [vim.diagnostic.severity.HINT] = '⚑',
+            [vim.diagnostic.severity.INFO] = '',
+        },
+        texthl = {
+            [vim.diagnostic.severity.ERROR] = '✘',
+            [vim.diagnostic.severity.WARN] = '▲',
+            [vim.diagnostic.severity.HINT] = '⚑',
+            [vim.diagnostic.severity.INFO] = '',
+        },
+        numhl = '',
+    }
+})
+
+function update_status_line() 
+    local warnings = #vim.diagnostic.get(nil, {severity = vim.diagnostic.severity.WARN})
+    local errors = #vim.diagnostic.get(nil, {severity = vim.diagnostic.severity.ERROR})
+
+    vim.o.statusline = "%f %#DiagnosticError#✘:" .. errors .. " %#DiagnosticWarn#▲:" .. warnings .. "%## %h%m%r%=%-14.(%l,%c%V%) %P"
+end
+
+vim.api.nvim_create_autocmd("DiagnosticChanged", {
+    callback = function() 
+      vim.diagnostic.setqflist({nr=1, open=false}, "r")
+      update_status_line()
+    end,
+})
+
